fd7c0ccf5f43dbadbf55c4a39fd28500586dc924
N. Xaplanteris <xaplanterisnikos@gmail.com>
2024-06-02 03:37:20 +0300

    Cross Origin

--- src/main/java/com/xaplanterisnikos/guitarshop/dao/ProductCategoryRepository.java
+++ src/main/java/com/xaplanterisnikos/guitarshop/dao/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.xaplanterisnikos.guitarshop.dao;
 import com.xaplanterisnikos.guitarshop.model.ProductCategory;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory",path ="product-category")
 public interface ProductCategoryRepository extends CrudRepository<ProductCategory, Long> {
 }
